1111d6da1054e2ad358d8b1662f577a98126d6fc
craiglathrop <clathrop@coldborecapital.com>
2024-06-17 13:07:16 -0700

    Update version in setup.py
    Updated the version of the cdbt CLI tool from 0.3.13 to 0.4.0 in the setup.py file. This change signifies the next stage in the tool's progress, including necessary updates and improvements.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="cdbt",
-    version="0.3.13",
+    version="0.4.0",
     description="A CLI tool to manage dbt builds with state handling and manifest management",
     author="Craig Lathrop",
     author_email="development@coldborecapital.com",
