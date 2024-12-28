926935708d4461d0f5f3dc1528776b728830e547
craiglathrop <clathrop@coldborecapital.com>
2024-12-28 10:39:52 -0700

    Update package version to 0.4.19 in setup.py
    Bump the version from 0.4.16 to 0.4.19 to reflect the latest changes. This prepares the package for a new release with any updates or fixes included since the last version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="cdbt",
-    version="0.4.16",
+    version="0.4.19",
     description="A CLI tool to manage dbt builds with state handling and manifest management",
     author="Craig Lathrop",
     author_email="development@coldborecapital.com",
